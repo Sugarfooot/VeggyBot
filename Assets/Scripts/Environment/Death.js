2e8e3f8a9a176d39346e83e4f572da737a834f27
Yann3D <yannick.jouneau3d@free.fr>
2016-10-07 16:46:46 +0200

    Checkpoints + Death + PlayerManager

---
+++ Assets/Scripts/Environment/Death.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter (collider : Collider){
+	if (collider.CompareTag("Player")){
+		collider.GetComponent.<PlayerManager>().Respawn();
+	}
+}
---
+++ Assets/Scripts/Player/PlayerManager.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+private var spawnPoint : Transform;
+
+function Start () {
+	spawnPoint.position = transform.position;
+}
+
+function Update () {
+
+}
+
+function Respawn (){
+	transform.position = spawnPoint.position;
+}
+
+function SetNewSpawnPoint (){
+	spawnPoint.position = transform.position;
+}
