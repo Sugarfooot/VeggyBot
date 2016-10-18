be321e5a3ead2bf207c44fb41a0508f0da04dad3
Yann3D <yannick.jouneau3d@free.fr>
2016-10-18 17:40:16 +0200

    Ajout d'une plateforme mouvante : ne fonctionne pas avec InVector actuellement.

--- Assets/scenes/TestsYannick/PlateformeMouvante.js
+++ Assets/scenes/TestsYannick/PlateformeMouvante.js
@@ -2,31 +2,34 @@
 
 var plateformeMouvante : boolean = false;
 var speed : float = 2.0;
+var tempsDAttente : float = 2.0;
 var waypoints : Transform;
-private var waypointsIdx : int = -1;
+private var localRB : Rigidbody;
+private var waypointsIdx : int = 0;
 private var move : boolean = false;
 
 function Start () {
+	localRB = GetComponent.<Rigidbody>();
 	if (plateformeMouvante){
 		waypoints.SetParent(null);
 		SetTargetWaypoint();
-		SwitchOn();
 	}
 }
 
-function Update () {
+function Update (){
+
+}
+
+function FixedUpdate () {
+	var localFwd : Vector3;
+	if (waypointsIdx == 0){
+		localFwd = waypoints.GetChild(waypointsIdx).position - waypoints.GetChild(waypoints.childCount - 1).position;
+	}
+	else if (waypointsIdx != 0){
+		localFwd = waypoints.GetChild(waypointsIdx).position - waypoints.GetChild(waypointsIdx - 1).position;
+	}
 	if (move && plateformeMouvante){
-		if (transform.position != waypoints.GetChild(waypointsIdx).position){
-			transform.position = Vector3.MoveTowards(transform.position,waypoints.GetChild(waypointsIdx).position,speed * Time.deltaTime);
-		}
-		else if (transform.position == waypoints.GetChild(waypointsIdx).position){
-			if (waypointsIdx == waypoints.childCount - 1){
-				ResetTargetWaypoints();
-			}
-			else if (waypointsIdx < waypoints.childCount - 1){
-				SetTargetWaypoint();
-			}
-		}
+		localRB.MovePosition(transform.position + localFwd.normalized * speed * Time.deltaTime);
 	}
 }
 
@@ -39,9 +42,24 @@ function SwitchOff (){
 }
 
 function SetTargetWaypoint (){
-	waypointsIdx += 1;
+	waypoints.GetChild(waypointsIdx).gameObject.GetComponent.<Collider>().enabled = false;
+	SwitchOff();
+	yield WaitForSeconds(tempsDAttente);
+	if (waypointsIdx == waypoints.childCount - 1){
+		waypointsIdx = 0;
+	}
+	else if (waypointsIdx < waypoints.childCount - 1){
+		waypointsIdx += 1;
+	}
+	waypoints.GetChild(waypointsIdx).gameObject.GetComponent.<Collider>().enabled = true;
+	SwitchOn();
 }
 
-function ResetTargetWaypoints (){
-	waypointsIdx = 0;
+function OnTriggerEnter (collider : Collider){
+	for (var i = 0; i < waypoints.childCount; i++){
+		if (collider.gameObject == waypoints.GetChild(i).gameObject){
+			SetTargetWaypoint();
+			return;
+		}
+	}
 }
