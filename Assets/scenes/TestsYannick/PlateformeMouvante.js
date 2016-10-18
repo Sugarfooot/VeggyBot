02641f8084e284cf4cd36ede718512dabe4b800d
Yann3D <yannick.jouneau3d@free.fr>
2016-10-18 15:39:46 +0200

    script de plateforme mouvante

---
+++ Assets/scenes/TestsYannick/PlateformeMouvante.js
@@ -0,0 +1,47 @@
+﻿#pragma strict
+
+var plateformeMouvante : boolean = false;
+var speed : float = 2.0;
+var waypoints : Transform;
+private var waypointsIdx : int = -1;
+private var move : boolean = false;
+
+function Start () {
+	if (plateformeMouvante){
+		waypoints.SetParent(null);
+		SetTargetWaypoint();
+		SwitchOn();
+	}
+}
+
+function Update () {
+	if (move && plateformeMouvante){
+		if (transform.position != waypoints.GetChild(waypointsIdx).position){
+			transform.position = Vector3.MoveTowards(transform.position,waypoints.GetChild(waypointsIdx).position,speed * Time.deltaTime);
+		}
+		else if (transform.position == waypoints.GetChild(waypointsIdx).position){
+			if (waypointsIdx == waypoints.childCount - 1){
+				ResetTargetWaypoints();
+			}
+			else if (waypointsIdx < waypoints.childCount - 1){
+				SetTargetWaypoint();
+			}
+		}
+	}
+}
+
+function SwitchOn (){
+	move = true;
+}
+
+function SwitchOff (){
+	move = false;
+}
+
+function SetTargetWaypoint (){
+	waypointsIdx += 1;
+}
+
+function ResetTargetWaypoints (){
+	waypointsIdx = 0;
+}
