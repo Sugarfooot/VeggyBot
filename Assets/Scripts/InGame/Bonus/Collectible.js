a4fea4611c1a4549cb93d248cedc2b827af4fb2e
Yann3D <yannick.jouneau3d@free.fr>
2016-10-27 18:02:30 +0200

    Ajout du niveau d'eau sur le robot + Intégration de la vie (rouages) WIP + Ajout des collectibles WIP

---
+++ Assets/Scripts/InGame/Bonus/Collectible.js
@@ -0,0 +1,49 @@
+﻿#pragma strict
+
+@script RequireComponent(BoxCollider)
+
+var rouage : boolean = false;
+var eau25 : boolean = false;
+var eau50 : boolean = false;
+var eau100 : boolean = false;
+
+function Start () {
+	GetComponent.<BoxCollider>().isTrigger = true;
+	if (rouage){
+		eau25 = false;
+		eau50 = false;
+		eau100 = false;
+	}
+	else if (eau25){
+		eau50 = false;
+		eau100 = false;
+	}
+	else if (eau50){
+		eau100 = false;
+	}
+	if (!rouage && !eau25 && !eau50 && !eau100){
+		eau25 = true;
+	}
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter (collider : Collider){
+	if (collider.CompareTag("Player")){
+		if (rouage){
+			BonusManager.Instance().AddLifePart();
+		}
+		if (eau25){
+			PlayerManager.Instance().AddWaterLevel(0.25);
+		}
+		if (eau50){
+			PlayerManager.Instance().AddWaterLevel(0.5);
+		}
+		if (eau100){
+			PlayerManager.Instance().AddWaterLevel(1.0);
+		}
+		Destroy(gameObject,Time.deltaTime);
+	}
+}
--- Assets/Scripts/Player/PlayerManager.js
+++ Assets/Scripts/Player/PlayerManager.js
@@ -1,19 +1,83 @@
 ﻿#pragma strict
 
-private var spawnPoint : Vector3;
+public class PlayerManager extends MonoBehaviour {
 
-function Start () {
-	spawnPoint = transform.position;
-}
+	var waterIndicators : Image[];
+	private var isShooting : boolean = false;
+	private var spawnPoint : Vector3;
+	private var waterLevel : float = 0;
+	var shootConsumptionSpeed : float = 0.1;
+	var vies : Image[];
+	private var vieIdx : int = 0;
+	static var maxVieIdx : int = 0;
 
-function Update () {
+	private static var instance : PlayerManager;
+	public static function Instance () : PlayerManager {
+		return instance;
+	}
 
-}
+	function Awake (){
+		if (instance != null){
+			Destroy (gameObject);
+		}
+		else {
+			instance = this;
+		}
+	}
 
-function Respawn (){
-	transform.position = spawnPoint;
-}
+	function Start () {
+		spawnPoint = transform.position;
+		vieIdx = maxVieIdx;
+	}
 
-function SetNewSpawnPoint (){
-	spawnPoint = transform.position;
+	function Update () {
+		if (Input.GetButtonDown("RB")){
+			TriggerWaterConsumption();
+		}
+		if (Input.GetButton("RB")){
+			if (waterLevel < 0){
+				waterLevel = 0;
+			}
+			if (isShooting && waterLevel > 0){
+				waterLevel -= shootConsumptionSpeed * Time.deltaTime;
+				for (var i = 0; i < waterIndicators.Length; i++){
+					waterIndicators[i].fillAmount = waterLevel;
+				}
+			}
+		}
+	}
+
+	function Respawn (){
+		transform.position = spawnPoint;
+		// !! AJOUTER LE CHARGEMENT DES PLAYERPREFS AU MOMENT DU SPAWN !!
+	}
+
+	function SetNewSpawnPoint (){
+		spawnPoint = transform.position;
+	}
+
+	function AddWaterLevel (quantity : float){
+		waterLevel += quantity;
+		if (waterLevel > 1){
+			waterLevel = 1;
+		}
+		for (var i = 0; i < waterIndicators.Length; i++){
+			waterIndicators[i].fillAmount = waterLevel;
+		}
+	}
+
+	function TriggerWaterConsumption (){
+		yield WaitForSeconds (0.1);
+		isShooting = true;
+	}
+
+	function OnTriggerEnter (collider : Collider){
+		if (collider.CompareTag("Damage")){
+			vieIdx --;
+			vies[vieIdx].gameObject.SetActive(false);
+			if(vieIdx == -1){
+				Respawn();
+			}
+		}
+	}
 }
