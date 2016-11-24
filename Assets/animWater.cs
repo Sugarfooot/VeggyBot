525698a3205337cc8a89fd70323c19038e08f701
Sugarfooot <arthur.glemee@gmail.com>
2016-11-24 09:35:06 +0100

    Test animation des cornichons + test Substance archives

---
+++ Assets/animWater.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class liquidEnergyAnimC : MonoBehaviour {
+
+	public ProceduralMaterial substance;
+	public float _animWater = 1;
+
+	void Start()
+	{
+		substance = GetComponent<Renderer>().sharedMaterial as ProceduralMaterial ;        
+	}
+
+	void LateUpdate()
+	{        
+		_animWater = _animWater + (2 * Time.smoothDeltaTime); 
+		substance.SetProceduralFloat("animWater", _animWater);
+		substance.RebuildTextures();
+	}
+}
