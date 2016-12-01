487d86ceb52f20db06ead1f7124873a70d9178a8
Yann3D <yannick.jouneau3d@free.fr>
2016-12-01 18:07:10 +0100

    Panneau électrique programmé. Proto OK, particules à rajouter + autres effets si nécessaire.

---
+++ Assets/Scripts/PanneauElectrique.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+var panelHP : int;
+var wastedPanel : GameObject;
+var activePanel : MeshRenderer;
+var isWet : boolean = false;
+
+function Start () {
+
+}
+
+function Update () {
+	if (isWet){
+		panelHP -= Time.deltaTime;
+		if (panelHP <= 0){
+			TriggerDeactivation();
+		}
+	}
+
+	isWet = false;
+}
+
+function OnParticleCollision (particles : GameObject){
+	if (particles.CompareTag("WaterPlayer")){
+		isWet = true;
+	}
+}
+
+function TriggerDeactivation (){
+	GetComponent.<Collider>().enabled = false;
+	activePanel.enabled = false;
+	wastedPanel.SetActive(true);
+}
