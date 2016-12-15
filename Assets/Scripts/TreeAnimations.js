6ed9f665d96002a933b0b23c8b36ed479e54c4a6
Yann3D <yannick.jouneau3d@free.fr>
2016-12-15 16:26:24 +0100

    Modif Cornichon

---
+++ Assets/Scripts/TreeAnimations.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+private var rndDelay : float = 4.0;
+
+function Start () {
+	yield WaitForSeconds (Random.Range(0.1, rndDelay));
+	GetComponent.<Animation>().Play();
+}
+
+function Update () {
+
+}
